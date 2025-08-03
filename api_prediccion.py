451e7729511c398d82bc01e8b9a0e8ef53657e8b
lsburbano <lsburbano@uce.edu.ec>
2025-08-03 12:30:02 -0500

    refactor: usar variable de entorno para la clave de OpenRouter

--- api_prediccion.py
+++ api_prediccion.py
@@ -13,6 +13,8 @@ client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY"),
 )
+print("API KEY:", os.getenv("OPENROUTER_API_KEY"))
+
 
 # === Cargar modelos SARIMA ===
 modelo_caudal = joblib.load("modelo_caudal.pkl")
